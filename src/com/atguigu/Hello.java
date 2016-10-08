3d6ebbc3388b29f5d2a54684de6bfab9dfd9bb01
zhaiqiwei <834551251@qq.com>
2016-10-08 21:04:09 +0800

    update02
    02

--- src/com/atguigu/Hello.java
+++ src/com/atguigu/Hello.java
@@ -4,6 +4,8 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("艾特尚硅谷");
-		System.out.println("aaaaaaaaa");
+		System.out.println("张金盆是个智障");
+		System.out.println("哈哈哈啊哈啊");
 	}
+
 }
